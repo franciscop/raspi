1ea4298bef85be80015cf13a92a91a593d2420a9
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-05-09 14:09:30 +0200

    Added camera 'stream'

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const server = require('server');
 const { get, socket } = server.router;
 const motor = require('./motor');
+const webcam = require('node-webcam');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const cam = webcam.create({ width: 640, height: 480 });
+const temp = path.join(os.tmpdir(), 'demo.jpg');
 
 const motorL = motor(0, 2);
 const motorR = motor(4, 5);
 
 server({}, [
   get('/', ctx => ctx.res.render('index')),
+  socket('init', async ctx => {
+    setInterval(() => {
+      cam.capture(temp, function(err, data) {
+        const file = fs.readFileSync(temp);
+        ctx.socket.emit('frame', { image: true, buffer: file.toString('base64') });
+      });
+    }, 1000);
+  }),
   socket('left', async ctx => {
     console.log('LEFT');
     await Promise.all([
@@ -21,15 +35,22 @@ server({}, [
       motorR.backward()
     ]);
   }),
-  socket('up', async ctx => {
+  socket('forward', async ctx => {
     console.log('FORWARD');
     await Promise.all([
       motorL.forward(),
       motorR.forward()
     ]);
   }),
-  socket('down', async ctx => {
+  socket('backward', async ctx => {
     console.log('BACKWARD');
+    await Promise.all([
+      motorL.backward(),
+      motorR.backward()
+    ]);
+  }),
+  socket('stop', async ctx => {
+    console.log('STOP');
     await Promise.all([
       motorL.stop(),
       motorR.stop()
--- public/javascript.js
+++ public/javascript.js
@@ -1,11 +1,78 @@
 const socket = io();
 
-dom.class.goup.on.click = e => socket.emit('up');
-dom.class.goleft.on.click = e => socket.emit('left');
-dom.class.goright.on.click = e => socket.emit('right');
-dom.class.godown.on.click = e => socket.emit('down');
+const joystick = document.querySelector('.joystick');
+const position = document.querySelector('.position');
 
-Mousetrap.bind('left', e => socket.emit('left'));
-Mousetrap.bind('right', e => socket.emit('right'));
-Mousetrap.bind('up', e => socket.emit('up'));
-Mousetrap.bind('down', e => socket.emit('down'));
+const event = name => {
+  console.log(name);
+  position.className = 'position ' + name;
+  return socket.emit(name);
+};
+
+Mousetrap.bind('left', e => event('left'));
+Mousetrap.bind('right', e => event('right'));
+Mousetrap.bind('up', e => event('forward'));
+Mousetrap.bind('down', e => event('backward'));
+
+// Render the images in the <img> element
+const img = document.querySelector('img');
+socket.emit('init');
+socket.on('frame', e => {
+  if (e.image) {
+    img.src = 'data:image/jpeg;base64,' + e.buffer;
+    // ctx.drawImage(img, 0, 0);
+  }
+});
+
+
+const controller = (sel, cb) => {
+  let start = false;
+
+  const begin = e => {
+    joystick.classList.remove('stop');
+    const client = e.clientX ? e : e.touches[0];
+    start = { x: client.clientX, y: client.clientY }
+  };
+  joystick.addEventListener('mousedown', begin);
+  joystick.addEventListener('touchstart', begin);
+
+  const end = e => {
+    joystick.classList.add('stop');
+    joystick.setAttribute('style', '');
+    start = false;
+    cb({ x: 0, y: 0 });
+  };
+  document.addEventListener('mouseup', end);
+  document.addEventListener('touchend', end);
+
+  const control = e => {
+    // Mousemove WHILE pressing it (not any other time)
+    if (!start) return;
+
+    const client = e.clientX ? e : e.touches[0];
+    const diff = {
+      x: client.clientX - start.x,
+      y: start.y - client.clientY
+    };
+    const divisor = 2;
+    //joystick.style.marginLeft = diff.x;
+    joystick.setAttribute('style', 'margin-left: ' + (diff.x / divisor) + 'px; margin-top: ' + (-diff.y / divisor) + 'px;');
+    cb(diff);
+  };
+  document.addEventListener('mousemove', control);
+  document.addEventListener('touchmove', control);
+};
+
+controller('.joystick', diff => {
+
+  // If similar to 0, it means stop
+  if (Math.abs(diff.x) < 20 && Math.abs(diff.y) < 20) {
+    return event('stop');
+  }
+
+  if (Math.abs(diff.x) > Math.abs(diff.y)) {
+    return event(diff.x > 0 ? 'right' : 'left');
+  }
+
+  return event(diff.y > 0 ? 'forward' : 'backward');
+});
