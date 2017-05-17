8b30604ca68a94b571a87dee67a7c0f85de73242
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-05-18 00:08:07 +0200

    Rotated

--- camera.js
+++ camera.js
@@ -3,6 +3,6 @@ const { exec } = require('mz/child_process');
 // Take a picture in an async way and return it as a base64 encoded string
 // Props: https://scottlinux.com/2012/09/01/encode-or-decode-base64-from-the-command-line/
 module.exports = async ({ resolution = '640x480' } = {}) => {
-  const out = await exec(`fswebcam -r ${resolution} - | base64`, { maxBuffer: 1024 * 1024 });
+  const out = await exec(`fswebcam -r ${resolution} --rotate 90 - | base64`, { maxBuffer: 1024 * 1024 });
   return out[0];
 };
